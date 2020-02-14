fac9de5e3c3caa80b7c1822e8ec87c449f919b7f
Horváth Gellért <gellerthorvath0@gmail.com>
2020-02-14 10:46:31 +0100

    Haladunk diagramok felé

---
+++ Forma1/Repository/F1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forma1.Repository
+{   /// <summary>
+/// A csapat neve
+/// </summary>
+    class F1
+    {
+        /// <summary>
+        /// Az F1 csapatok
+        /// </summary>
+        List<Team> teams;
+        /// <summary>
+        /// Konstruktor
+        /// </summary>
+        public F1()
+        {
+            teams = new List<Team>();
+        }
+        /// <summary>
+        /// Létrehozz egy új csapatott az F1-ben
+        /// </summary>
+        /// <param name="name"> Új csapat neve</param>
+        public void add(string name)
+        {
+            Team t = new Team(name);
+            teams.Add(t);
+        }
+
+
+
+
+    }
+}
---
+++ Forma1/Repository/Team.cs
@@ -0,0 +1,35 @@
+﻿using Forma1.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forma1.Repository
+{
+    /// <summary>
+    /// A Forma 1-ben versenyző csapatok
+    /// </summary>
+    class Team
+    {
+        /// <summary>
+        /// Versenyző neve
+        /// </summary>
+        private string name;
+        /// <summary>
+        /// Racer Lista elkészítése
+        /// </summary>
+        List<Racer> racers;
+        /// <summary>
+        /// Forma 1 csapat csak névvel jöhet létre
+        /// </summary>
+        /// <param name="name"></param>
+        public Team(string name)
+        {
+            this.name = name;
+            racers = new List<Racer>();
+        }
+
+
+    }
+}
